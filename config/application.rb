da9c2459e3d8f93211b506d09eb8a48c58637e05
zumi-n <zumi0369@gmail.com>
2019-03-19 20:23:43 +0900

    Edit user

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,10 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-    end
+    config.i18n.default_locale = :ja
+  end
+end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
 end
