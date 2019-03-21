25260caeec3d47a72ca2ba8ec7cae36a04f8f2f6
zumi-n <zumi0369@gmail.com>
2019-03-21 15:07:39 +0900

    Edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'messages#index'
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
 
 
