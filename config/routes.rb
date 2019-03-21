708b9e998081dda00ddde5cc86a67dbfb2b4a510
zumi-n <zumi0369@gmail.com>
2019-03-21 15:29:35 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
 end
-
-
-
-
-
-
