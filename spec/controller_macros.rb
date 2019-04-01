07803b2df3203510d61f1094dfc9612a45b23bdf
zumi-n <zumi0369@gmail.com>
2019-04-01 12:43:33 +0900

    Edit some

---
+++ spec/controller_macros.rb
@@ -0,0 +1,6 @@
+module ControllerMacros
+  def login(user)
+    @request.env["devise.mapping"] = Devise.mapping[:user]
+    sign_in user
+  end
+end
---
+++ spec/controllers/message_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe MessageController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'assigns @message' do
+        expect(assingns(:message)).to be_a_new(Message)
+      end
+
+      it 'assingns @group' do
+        expect(assingns(:group)).to eq group
+      end
+
+      it 'renders index' do
+        expect(response).to render_template :index
+      end
+    end
+
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
