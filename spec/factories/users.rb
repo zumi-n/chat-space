274d1c3847cf56c9b7a86227f7cee8e9d52d93b1
zumi-n <zumi0369@gmail.com>
2019-04-01 14:36:52 +0900

    Delete spec/factories

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    password = Faker::Internet.password(8)
-    name = {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password {password}
-    password_confirmation {password}
-  end
-end
