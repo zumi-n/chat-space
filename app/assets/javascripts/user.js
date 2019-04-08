d65e288809258ae90421382432a45ef54752af73
zumi-n <zumi0369@gmail.com>
2019-04-08 12:53:46 +0900

    Fix indent

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,33 +5,33 @@ $(function(){
 
   function appendUserToSearchList(user){
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-                </div>`
+    <p class="chat-group-user__name">${user.name}</p>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+    </div>`
     search_list.append(html);
   }
 
 
-    function appendNoUserToSearchList(user) {
+  function appendNoUserToSearchList(user) {
     var html =
-      `<div class="chat-group-user clearfix">
-        <p class="chat-group-user__name">${ user }</p>
-      </div>`
+    `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${ user }</p>
+    </div>`
     search_list.append(html);
   }
 
 
   function appendUserToMemberList(user_id, user_name) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id="${user_id}">
-                  <input name='group[user_ids][]' type='hidden' value="${user_id}">
-                  <p class='chat-group-user__name'>${user_name}</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-               </div>`
+    <input name='group[user_ids][]' type='hidden' value="${user_id}">
+    <p class='chat-group-user__name'>${user_name}</p>
+    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
     member_list.append(html);
   }
 
 
-    $("#user-search-field").on('keyup', function() {
+  $("#user-search-field").on('keyup', function() {
     var input = $("#user-search-field").val();
 
 
@@ -46,12 +46,12 @@ $(function(){
     .done(function(user) {
       $('#user-search-result').empty();
       if (user.length !== 0){
-      user.forEach(function(user){
-      appendUserToSearchList(user);
-      });
+        user.forEach(function(user){
+          appendUserToSearchList(user);
+        });
       }
       else {
-      appendNoUserToSearchList('一致するユーザーが見つかりません');
+        appendNoUserToSearchList('一致するユーザーが見つかりません');
       }
     })
 
@@ -60,11 +60,11 @@ $(function(){
     })
   });
 
-    $(document).on('click', '.user-search-add', function(){
-      $(this).parent()[0].remove();
-      var user_id = $(this).data("user-id");
-      var user_name = $(this).data("user-name");
-      appendUserToMemberList(user_id, user_name);
+  $(document).on('click', '.user-search-add', function(){
+    $(this).parent()[0].remove();
+    var user_id = $(this).data("user-id");
+    var user_name = $(this).data("user-name");
+    appendUserToMemberList(user_id, user_name);
 
     $(document).on("click", '.user-search-remove', function(){
       $(this).parent().remove();
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
       respond_to do |format|
         format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
         format.json
-    end
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
