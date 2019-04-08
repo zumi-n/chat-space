c8c6a87bbc3fd320292ec78ce4cf91b31e55a4ba
zumi-n <zumi0369@gmail.com>
2019-04-08 19:20:24 +0900

    完成版

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,56 +1,72 @@
-$(function(){
-  function buildHTML(message){
-    var insertImage = '';
-    if (message.image) {
-      insertImage = `<img src="${message.image}">`;
+  $(function(){
+    function buildHTML(message){
+      var insertImage = '';
+      if (message.image) {
+        insertImage = `<img src=${message.image}>`;
+      }
+      var html = `<div class="message" id=${message.id}>
+      <div class="upper-message">
+      <p class="upper-message__name">
+      ${ message.user_name }</p>
+      <p class="upper-message__date">
+      ${ message.created_at }</p>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+      ${ message.content }
+      </p>
+      <img class="lower-message__image">
+      ${ insertImage }
+      </div>
+      </div>`;
+      return html;
     }
-    var html = `<div class="message" id="${message.id}">
-    <div class="upper-message">
-    <p class="upper-message__name">
-    ${ message.user_name }</p>
-    <p class="upper-message__date">
-    ${ message.created_at }</p>
-    </div>
-    <div class="lower-message">
-    <p class="lower-message__content">
-    ${ message.content }
-    </p>
-    <img class="lower-message__image">
-    ${ insertImage }
-    </div>
-    </div>`;
-    return html;
-  }
 
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
+    function nullCheck(){
+     var check = true;
+     var text = $('.form__message').val().trim();
+     var image = $('.hidden').val().trim();
 
-    .done(function(message){
-      var html = buildHTML(message);
-      $('.messages').append(html)
-      $('#new_message')[0].reset()
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-      $('.form__submit').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('メッセージを入力してください');
-    })
+     if(text == "" && image == ""){
+       check = false;
+     }
+     return check;
+   }
+
+   $('#new_message').on('submit', function(e){
+    if (nullCheck() == false){
+     alert("メッセージを入力してください");
+     return false;
+   }
+
+   e.preventDefault();
+   var formData = new FormData(this);
+   var url = $(this).attr('action')
+   $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
+
+   .done(function(message){
+    var html = buildHTML(message);
+    $('.messages').append(html)
+    $('#new_message')[0].reset()
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+    $('.form__submit').prop('disabled', false);
+  })
+   .fail(function(){
+    alert('メッセージを入力してください');
+  })
+ })
 
   // 以下、自動更新機能
   setInterval(reloadMessages, 5000);
   function reloadMessages(){
-    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $('.message:last').attr('id');
       $.ajax({
         url: location.href,
@@ -73,3 +89,4 @@ $(function(){
     }
   };
 });
+
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,39 +1,38 @@
 $(function(){
 
-  var search_list = $("#user-search-result");
-  var member_list = $(".chat-group-user__name");
+  var searchlist = $('#user-search-result')
 
   function appendUserToSearchList(user){
     var html = `<div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${user.name}</p>
-    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
     </div>`
-    search_list.append(html);
+    searchlist.append(html);
   }
 
 
-  function appendNoUserToSearchList(user) {
+  function appendNoUserToSearchList(user){
     var html =
     `<div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${ user }</p>
     </div>`
-    search_list.append(html);
+    searchlist.append(html);
   }
 
+  var memberlist = $('#chat-group-users')
 
-  function appendUserToMemberList(user_id, user_name) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id="${user_id}">
-    <input name='group[user_ids][]' type='hidden' value="${user_id}">
-    <p class='chat-group-user__name'>${user_name}</p>
+  function appendUserToMemberList(id, name){
+    var html = `<div class='chat-group-user clearfix js-member'>
+    <input name='group[user_ids][]' type='hidden' value=${id}>
+    <p class='chat-group-user__name'>${name}</p>
     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
     </div>`
-    member_list.append(html);
+    memberlist.append(html);
   }
 
 
-  $("#user-search-field").on('keyup', function() {
-    var input = $("#user-search-field").val();
-
+  $("#user-search-field.chat-group-form__input").on('keyup', function() {
+    var input = $("#user-search-field.chat-group-form__input").val();
 
     $.ajax({
       type: 'GET',
@@ -42,33 +41,29 @@ $(function(){
       dataType: 'json'
     })
 
-
-    .done(function(user) {
+    .done(function(users) {
       $('#user-search-result').empty();
-      if (user.length !== 0){
-        user.forEach(function(user){
-          appendUserToSearchList(user);
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUserToSearchList(user)
         });
-      }
-      else {
+      } else {
         appendNoUserToSearchList('一致するユーザーが見つかりません');
       }
     })
-
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     })
   });
-
   $(document).on('click', '.user-search-add', function(){
     $(this).parent()[0].remove();
     var user_id = $(this).data("user-id");
     var user_name = $(this).data("user-name");
     appendUserToMemberList(user_id, user_name);
+  });
 
-    $(document).on("click", '.user-search-remove', function(){
-      $(this).parent().remove();
-    });
-  })
+  $(document).on("click", '.user-search-remove', function(){
+    $(this).parent().remove();
+  });
 });
 
